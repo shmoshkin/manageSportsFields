a6d964e214a031e41d245dbdcebff11e8f3c6356
amit shmoshkin <shmoshkin@gmail.com>
2017-09-24 23:41:27 +0300

    add services folder and bind name and description into field html

--- fieldsManagement/src/app/components/field/field.component.ts
+++ fieldsManagement/src/app/components/field/field.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {materialize, Materialize} from 'materialize-css'  
+import {Injectable, Component, OnInit } from '@angular/core';
+import {materialize, Materialize} from 'materialize-css'
+
 @Component({
   selector: 'app-field',
   templateUrl: './field.component.html',
   styleUrls: ['./field.component.scss']
 })
+@Injectable()
 export class FieldComponent implements OnInit {
-  constructor() { }
-
-  id: number;
-  location: string;
-  name: string;
-  type: number;
-  imgUrl: string;
-  available: boolean;
+  
+  private id: number;
+  private location: string;
+  private name: string;
+  private type: number;
+  private imgUrl: string;
+  private available: boolean;
+  private description: string
+  
+  constructor() {} 
 
   ngOnInit() {
 
   }
 
-  onAddField(){
+  onAddField(){    
     
-    Materialize.Toast('I am a toast', 4000,'',function(){
-      alert('Your toast was dismissed')
-    })
+    // Add some code for open card
   }
 }
