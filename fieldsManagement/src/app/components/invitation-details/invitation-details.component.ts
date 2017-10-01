d4798b32af92b47a2baecdbdd8fe23bfeeb4532b
amit shmoshkin <shmoshkin@gmail.com>
2017-10-01 23:38:11 +0300

    change names of classes is scss and add field of location

--- fieldsManagement/src/app/components/invitation-details/invitation-details.component.ts
+++ fieldsManagement/src/app/components/invitation-details/invitation-details.component.ts
@@ -11,19 +11,22 @@ export class InvitationDetailsComponent implements OnInit {
   
   fieldName: string;
   times: string;
+  location: string;
   toastActions = new EventEmitter<string|MaterializeAction>();
-  
+  maxLength = 20;
+
   constructor() 
   {
     this.times = "10 - 12 am, 12 march 2017";
     this.fieldName = "rabin - field 1";
+    this.location = "hapashosh street 54"
   }
 
   ngOnInit() {
   }
 
   onCancelInvitation(){
-
+    
   }
 
   onAgreeInvitation(){
